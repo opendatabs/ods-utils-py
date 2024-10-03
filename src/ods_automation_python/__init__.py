b899ddbd6b2b0fa2f0e1dd10c93e16577a2ee9f5
Renato Farruggio <renato.farruggio@bluewin.ch>
2024-10-03 14:52:04 +0200

    Load proxy info from .env file, update how completeness from .env is checked, update how functions can be imported.

--- src/ods_automation_python/__init__.py
+++ src/ods_automation_python/__init__.py
@@ -1,4 +1,13 @@
-from ._config import _get_api_key
+import pkgutil
+import importlib
 
-# Just to make sure that the key is specified in the .env file, and immediately give an error if it is not
-_get_api_key()
+from ._config import _check_all_environment_variables_are_set
+
+_check_all_environment_variables_are_set()
+
+# Automatically import all modules in this package
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
+    module = importlib.import_module(f"{__name__}.{module_name}")
+    # Import all functions defined in __all__ from the module
+    if hasattr(module, '__all__'):
+        globals().update({name: getattr(module, name) for name in module.__all__})
--- src/ods_automation_python/_config.py
+++ src/ods_automation_python/_config.py
@@ -1,22 +1,39 @@
 import os
+
 from dotenv import load_dotenv
 
+load_dotenv(".env")
+if not os.path.exists(".env"):
+    raise FileNotFoundError("The .env file does not exist.")
 
-def _get_api_key(env_file=".env"):
+def _check_all_environment_variables_are_set():
+    environment_variables = ["API_KEY",
+                             "PROXY_USER",
+                             "PROXY_PASSWORD",
+                             "PROXY_ADDRESS",
+                             "PROXY_PORT"]
 
-    # Load the environment variables from the specified .env file
-    if not os.path.exists(env_file):
-        raise FileNotFoundError(f"The .env file '{env_file}' does not exist.")
+    for environment_variable in environment_variables:
+        ev = os.getenv(environment_variable)
+        if not ev:
+            raise ValueError(f"{environment_variable} not found in the .env file. "
+                             f"Please define it as '{environment_variable}'.")
 
-    load_dotenv(env_file)
+def _get_api_key():
+    return os.getenv('API_KEY')
 
-    # Retrieve the API key from the environment variables
-    api_key = os.getenv('API_KEY')
-    if not api_key:
-        raise ValueError("API key not found in the .env file. Please define it as 'API_KEY'.")
+def _get_proxies() -> dict[str, str]:
+    proxy_user = os.getenv("PROXY_USER")
+    proxy_password = os.getenv("PROXY_PASSWORD")
+    proxy_address = os.getenv("PROXY_ADDRESS")
+    proxy_port = os.getenv("PROXY_PORT")
 
-    return api_key
-
+    proxy = f"http://{proxy_user}:{proxy_password}@{proxy_address}:{proxy_port}/"
+    proxies = {
+        "http": proxy,
+        "https": proxy,
+    }
+    return proxies
 
 # Example of how to use the function
 if __name__ == "__main__":
