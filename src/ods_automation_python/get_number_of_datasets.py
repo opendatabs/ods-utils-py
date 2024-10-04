b0251e66df46fd65c4dbf032c2d4f2d8e32e1daa
Renato Farruggio <renato.farruggio@bluewin.ch>
2024-10-04 09:55:03 +0200

    Add comment

--- src/ods_automation_python/get_number_of_datasets.py
+++ src/ods_automation_python/get_number_of_datasets.py
@@ -1,6 +1,7 @@
 from ._requests_utils import *  # imports requests_get, requests_post, etc.
 from ._config import get_base_url
 
+# Add the methods that are meant for public use in __all__
 __all__ = ["get_number_of_datasets"]
 
 def get_number_of_datasets():
