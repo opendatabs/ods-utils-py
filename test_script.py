62e9213376a7a4f2efe4b2c08d784ff371661324
Renato Farruggio <renato.farruggio@bluewin.ch>
2024-10-04 13:59:47 +0200

    Clean up cleanup leftovers

--- test_script.py
+++ test_script.py
@@ -1,8 +1,8 @@
-import ods_automation_python
+import ods_utils_py as ods_utils
 
 def main():
     print("Trying to receive count of all datasets")
-    total_num = ods_automation_python.get_number_of_datasets()
+    total_num = ods_utils.get_number_of_datasets()
     print("Total number of datasets:", total_num)
 
 if __name__ == "__main__":
