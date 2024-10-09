6dc928d6dbe57e6a53ddd5ff7a81c58a9ac0c59b
Renato Farruggio <renato.farruggio@bluewin.ch>
2024-10-09 11:52:44 +0200

    Update how public methods are handled from automatic to manually, to remove a warning and for overall readability.
    Now the __init__.py file has to be updated, but the exports are clear to the user and the IDE type checker.

--- src/ods_utils_py/__init__.py
+++ src/ods_utils_py/__init__.py
@@ -1,13 +1,7 @@
-import pkgutil
-import importlib
-
 from ._config import _check_all_environment_variables_are_set
 
 _check_all_environment_variables_are_set()
 
-# Automatically import all modules in this package
-for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-    module = importlib.import_module(f"{__name__}.{module_name}")
-    # Import all functions defined in __all__ from the module
-    if hasattr(module, '__all__'):
-        globals().update({name: getattr(module, name) for name in module.__all__})
+
+### Public Methods ###
+from .get_number_of_datasets import get_number_of_datasets
--- src/ods_utils_py/get_number_of_datasets.py
+++ src/ods_utils_py/get_number_of_datasets.py
@@ -1,9 +1,7 @@
 from ._requests_utils import *  # imports requests_get, requests_post, etc.
 from ._config import get_base_url
 
-# Add the methods that are meant for public use in __all__
-__all__ = ["get_number_of_datasets"]
-
 def get_number_of_datasets():
-    r = requests_get(url=f"{get_base_url()}/datasets/")
+    base_url = get_base_url()
+    r = requests_get(url=f"{base_url}/datasets/")
     return r.json()['total_count']
