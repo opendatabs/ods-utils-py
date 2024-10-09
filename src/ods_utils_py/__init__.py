5b8e5eb5278bd0e1e000d7d5de8c54b91ac5ca64
Renato Farruggio <renato.farruggio@bluewin.ch>
2024-10-09 15:34:07 +0200

    Add get_dataset_custom_view.py

--- src/ods_utils_py/__init__.py
+++ src/ods_utils_py/__init__.py
@@ -6,3 +6,4 @@ _check_all_environment_variables_are_set()
 ### Public Methods ###
 from .get_number_of_datasets import get_number_of_datasets
 from .get_uid_by_id import get_uid_by_id
+from .get_dataset_custom_view import get_dataset_custom_view
