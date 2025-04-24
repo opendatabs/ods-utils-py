88cb95b5109f5e19a7959e05b5d9a1eab36b3ef8
RenatoFarruggio <renato.farruggio@bluewin.ch>
2025-04-24 10:24:54 +0200

    Update to Version 0.1.22

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="ods_utils_py",
-    version="0.1.21",
+    version="0.1.22",
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     description="A python wrapper library for the ODS Automation API",
